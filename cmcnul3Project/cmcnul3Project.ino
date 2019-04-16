7bd29740495ec09f04c946e9ef8bd03e1d19ccbf
Carlos McNulty <cmcnul3@uic.edu>
2019-04-16 00:39:48 -0500

    improved user interface

--- cmcnul3Project/cmcnul3Project.ino
+++ cmcnul3Project/cmcnul3Project.ino
@@ -1,3 +1,4 @@
+
 #include <LiquidCrystal.h>
 #include <string.h>
 #include <Wire.h>
@@ -12,10 +13,6 @@ int programState = OFF;
 
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-void scrollPrint(int r, int c, char *str){
-  
-}
-
 void setup(){
   Wire.begin();
   pinMode(upButtonPin, INPUT);
@@ -28,6 +25,10 @@ void setup(){
   lcd.blink();
 }
 
+/* This function checks whether a button was pressed
+ * and will only return true when pressed, but not
+ * while being pressed.
+ */
 int buttonPressed(int buttonPin, int *buttonState){
   if(!buttonState){return -1;}
   int before = digitalRead(buttonPin);
@@ -44,84 +45,105 @@ int buttonPressed(int buttonPin, int *buttonState){
   return 0;
 }
 
-void getTimeFromLCD(int *hour, int *minutes, int *seconds){
-  if(!hour || !minutes || !seconds){ return; }
+/* This function uses an lcd and two buttons as a user interface
+ * to get the current time from the user.
+ */
+void getTimeFromLCD(int *hour, int *minute, int *second){
+  if(!hour || !minute || !second){ return; }
   lcd.clear();
-	char buffer[100];
-	char *format = "00:00:00";
-  lcd.print(format);
-	lcd.setCursor(0,0);
+  lcd.setCursor(0,0);
+  lcd.print("00:00:00");
+  *hour = 0;
+  *minute = 0;
+  *second = 0;
+	char buffer[50];
 	int complete = 0;
+  // get the hours from the user
 	while(!complete){
-		char *format = "%02d:00:00";
-		if(buttonPressed(enterButtonPin, &enterButtonState)){
-			complete = 1;
-		}
-		else{
-			if(buttonPressed(upButtonPin, &upButtonState)){
-				if(*hour + 1 <= 23){
-					*hour++;
-				}
-			}
-			if(buttonPressed(downButtonPin, &downButtonState)){
-				if(*hour - 1 >= 0){
-					*hour--;
-				}
-			}
-		}
-		snprintf(buffer, 100, format, *hour);
-		lcd.print(buffer);
-	}
-	complete = 0;
-	lcd.setCursor(3,0);
-	while(!complete){
-		char *format = "%02d:%02d:00";
-		if(buttonPressed(enterButtonPin, &enterButtonState)){
-			complete = 1;
-		}
-		else{
-			if(buttonPressed(upButtonPin, &upButtonState)){
-				if(*minutes + 1 <= 59){
-					*minutes++;
-				}
-			}
-			if(buttonPressed(downButtonPin, &downButtonState)){
-				if(*minutes - 1 >= 0){
-					*minutes--;
-				}
-			}
-		}
-		snprintf(buffer, 100, format, *hour, *minutes);
-		lcd.print(buffer);
-	}
-	complete = 1;
-	lcd.setCursor(6,0);
-	while(!complete){
+    lcd.setCursor(0,0);
 		char *format = "%02d:%02d:%02d";
 		if(buttonPressed(enterButtonPin, &enterButtonState)){
 			complete = 1;
 		}
 		else{
 			if(buttonPressed(upButtonPin, &upButtonState)){
-				if(*seconds + 1 <= 59){
-					*seconds++;
+        (*hour)++;
+        // hours wrap back around
+				if(*hour > 23){
+					*hour = 0;
 				}
 			}
 			if(buttonPressed(downButtonPin, &downButtonState)){
-				if(*seconds - 1 >= 0){
-					*seconds--;
+        (*hour)--;
+				if(*hour < 0){
+					*hour = 23;
 				}
 			}
 		}
-		snprintf(buffer, 100, format, *hour, *minutes, *seconds);
+		snprintf(buffer, 50, format, *hour, *minute, *second);
+		lcd.print(buffer);
+	}
+  complete = 0;
+  // get the minutes from the user
+	while(!complete){
+    lcd.setCursor(3,0);
+    // drop the hour portion of the format for current lcd cursor position
+		char *format = "%02d:%02d";
+		if(buttonPressed(enterButtonPin, &enterButtonState)){
+			complete = 1;
+		}
+		else{
+			if(buttonPressed(upButtonPin, &upButtonState)){
+        (*minute)++;
+        // minutes wrap back around
+				if(*minute >= 59){
+					*minute = 0;
+				}
+      }
+			if(buttonPressed(downButtonPin, &downButtonState)){
+        (*minute)--;
+				if(*minute < 0){
+					*minute = 59;
+				}
+			}
+		}
+		snprintf(buffer, 50, format, *minute, *second);
+		lcd.print(buffer);
+	}
+  complete = 0;
+  // get seconds from the user
+	while(!complete){
+    lcd.setCursor(6,0);
+    // drop hour and minute portion of format for currrent lcd cursor position
+		char *format = "%02d";
+		if(buttonPressed(enterButtonPin, &enterButtonState)){
+			complete = 1;
+		}
+		else{
+			if(buttonPressed(upButtonPin, &upButtonState)){
+        (*second)++;
+        // seconds wrap back around
+				if(*second > 59){
+					*second = 0;
+				}
+			}
+			if(buttonPressed(downButtonPin, &downButtonState)){
+        (*second)--;
+				if(*second < 0){
+					*second = 59;
+				}
+			}
+		}
+		snprintf(buffer, 50, format, *second);
 		lcd.print(buffer);
 	}
 }
 
-int validDate(int month, int day, int *year, int *max){
+int validDate(int month, int day, int year, int *max){
 	switch(month){
 		case 1: case 3: case 5: case 7:
 		case 8: case 10: case 12:{
+      // set the max day of the month
 			if(max){ *max = 31; } 
 			if(day >= 1 && day <= 31){
 				return 1;
@@ -137,8 +159,7 @@ int validDate(int month, int day, int *year, int *max){
 		}
 		case 2:{
 			// check for a leap year
-			if(year
-				&& (*year%400 == 0 || (*year%4 == 0 && *year%100 != 0))){
+			if(year%400 == 0 || (year%4 == 0 && year%100 != 0)){
 				// set leap year max
 				if(max){ *max = 29; }
 				if(day >= 1 && day <= 29){
@@ -146,7 +167,7 @@ int validDate(int month, int day, int *year, int *max){
 				}
 			}
 			else{
-				// without year default is no leap year
+				// default is no leap year
 				if(day >= 1 && day <= 28){
 					if(max){ *max = 28; }
 					return 1;
@@ -160,211 +181,232 @@ int validDate(int month, int day, int *year, int *max){
 		return 0;
 }
 
-
+/* This functions uses an lcd and two buttons as a user interface
+ * to get the current date from the user.
+ */
 void getDateFromLCD(int *month, int *day, int *year){
   if(!month || !day || !year){ return; }
   lcd.clear();
   lcd.setCursor(0,0); 
-	char *format = "00.00.00";
-	*month = 1;
-	*day = 1;
+  lcd.print("00.00.00");
+  // set default date to this year
+	*month = 4;
+	*day = 15;
 	*year = 18;
-  lcd.print(format);
+  char buffer[50];
 	int complete = 0;
+  // get the year from the user
   while(!complete){
-		char *format = "%02d.00.00";
+    lcd.setCursor(0,0);
+    char *format = "%02d.%02d.%02d";
+		if(buttonPressed(enterButtonPin, &enterButtonState)){
+		  complete = 1;
+		}
+		else{
+			if(buttonPressed(upButtonPin, &upButtonState)){
+				(*year)++;
+        // years wrap back around to 0 - 99
+				if(*year > 99){
+					*year = 0;
+				}
+			}
+			if(buttonPressed(downButtonPin, &downButtonState)){
+				(*year)--;
+				if(*year < 0){
+					*year = 99;
+				}
+			}
+		}
+		snprintf(buffer, 50, format, *year, *month, *day);
+		lcd.print(buffer);
+	}
+  complete = 0;
+  // get the month from the user
+  while(!complete){
+    lcd.setCursor(3,0);
+    // year portion of format dropped for new lcd cursor position
+    char *format = "%02d.%02d";
     if(buttonPressed(enterButtonPin, &enterButtonState)){
       complete = 1;
     }
     else{
       if(buttonPressed(upButtonPin, &upButtonState)){
-				*month++;
+				(*month)++;
+        // months wrap back around
 				if(*month > 12){
 					*month = 1;
 				}
       }
       if(buttonPressed(downButtonPin, &downButtonState)){
-				*month--;
+				(*month)--;
 				if(*month < 1){
 					*month = 12;
 				}
       }
     }
-		snprintf(buffer, 100, format, *month);
+		snprintf(buffer, 50, format, *month, *day);
 		lcd.print(buffer);
   }
 	complete = 0;
-	lcd.setCursor(3,0);
+  // get the day from the user
 	while(!complete){
-		char *format = "%02d.%02d.00";
+    lcd.setCursor(6, 0);
+    // year and month dropped from format
+    char *format = "%02d";
     if(buttonPressed(enterButtonPin, &enterButtonState)){
       complete = 1;
     }
     else{
       if(buttonPressed(upButtonPin, &upButtonState)){
-				*day++;
-				if(!validDate(*month, *day, NULL, NULL)){
+				(*day)++;
+        // days wrap back around to 1
+				if(!validDate(*month, *day, *year + 2000, NULL)){
 					*day = 1;
 				}
 			}
       if(buttonPressed(downButtonPin, &downButtonState)){
 				int max;
-				*day--;
-				if(!validDate(*month, *day, NULL, &max)){
+				(*day)--;
+        // days wrap back around to the max day for the given
+        // year and month (includes leap years)
+				if(!validDate(*month, *day, *year + 2000, &max)){
 					*day = max;
 				}
       }
     }
-		snprintf(buffer, 100, format, *month, *day);
-		lcd.print(buffer);
-	}
-	complete = 0;
-	lcd.setCursor(6, 0);
-	while(!complete){
-		char *format = "%02d.%02d.%02d";
-		if(buttonPressed(enterButtonPin, &enterButtonState)){
-			complete = 1;
-		}
-		else{
-			if(buttonPressed(upButtonPin, &upButtonState)){
-				int max;
-				*year++;
-				if(*year > 99){
-					*year = 0;
-				}
-				// add offset to year
-				*year += 2000;
-				if(!validDate(*month, *day, year, &max)){	
-					// check if day is on leap year
-					if(*day > max){
-						// set day to the 29th on leap year
-						*day = max;
-					}
-				}
-				// remove offset from year
-				*year -= 2000;
-			}
-			if(buttonPressed(downButtonPin, &downButtonState)){
-				int max;
-				*year--;
-				if(*year < 0){
-					*year = 99;
-				}
-				// add offset to year
-				*year += 2000;
-				if(!validDate(*month, *day, year, &max)){
-					// check if day is on leap year
-					if(*day > max){
-						// set day to the 29th on leap year
-						*day = max;
-					}
-				}
-				// remove offset from year
-				*year -= 2000;
-			}
-		}
-		snprintf(buffer, 100, format, *month, *day, *year);
+		snprintf(buffer, 50, format, *day);
 		lcd.print(buffer);
 	}
 }
 
-void getScheduleFromLCD(int *days, int *hours){
+/* This function uses an lcd and two buttons to get the 
+ * watering schedule from the user.
+ */
+void getScheduleFromLCD(int *days, int *hours, int *minutes){
   if(!days || !hours){ return; }
   lcd.clear();
   lcd.setCursor(0,0);
-  lcd.print("Enter schedule:");
-  lcd.setCursor(0,1);
-  lcd.print("00:00");
-  int tensPlace = 0, onesPlace = 0;
-  int i = 0;
-  while(i < 2){
-    lcd.setCursor(i,1);
+  lcd.print("00::00:00");
+  *days = 0;
+  *hours = 0;
+  *minutes = 0;
+  char buffer[50];
+  int complete = 0;
+  // get the days from the user
+  while(!complete){
+    lcd.setCursor(0,0);
+    char *format = "%02d::%02d:%02d";
     if(buttonPressed(enterButtonPin, &enterButtonState)){
-      i++;
+      complete = 1;
     }
     else{
       if(buttonPressed(upButtonPin, &upButtonState)){
-        if(i == 0 && tensPlace + 1 <= 3){
-          tensPlace++;
-          lcd.write('0' + tensPlace);
-        }
-        if(i == 1 && (onesPlace + 1) <= 9
-                  && (tensPlace * 10 + (onesPlace + 1)) <= 31){
-            onesPlace++;
-            lcd.write('0' + onesPlace);
+        (*days)++;
+        // days wrap back around 0 - 99
+        if(*days > 99){
+          *days = 0;
         }
       }
       if(buttonPressed(downButtonPin, &downButtonState)){
-        if(i == 0 && tensPlace - 1 >= 0){
-          tensPlace--;
-          lcd.write('0' + tensPlace);
-        }
-        if(i == 1 && onesPlace - 1 >= 0){
-          onesPlace--;
-          lcd.write('0' + onesPlace);
+        (*days)--;
+        if(*days < 0){
+          *days = 99;
         }
       }
     }
+    snprintf(buffer, 50, format, *days, *hours, *minutes);
+    lcd.print(buffer);
   }
-  *days = tensPlace * 10 + onesPlace; 
-  tensPlace = 0;
-  onesPlace = 0;
-  i++;
-  while(i < 5){
-    lcd.setCursor(i,1);
+  complete = 0;
+  while(!complete){
+    lcd.setCursor(4, 0);
+    // drop the days portion of the format for current lcd cursor position
+    char *format = "%02d:%02d";
     if(buttonPressed(enterButtonPin, &enterButtonState)){
-      i++;
+      complete = 1;
     }
     else{
       if(buttonPressed(upButtonPin, &upButtonState)){
-        if(i == 3 && tensPlace + 1  <= 2){
-          tensPlace++;
-          lcd.write('0' + tensPlace);
-        }
-        if(i == 4 && onesPlace + 1 <= 9
-                  && (tensPlace * 10 + (onesPlace + 1)) <= 23){
-          onesPlace++;
-          lcd.write('0' + onesPlace);
+        (*hours)++;
+        // hours wrap back around 0 - 99
+        if(*hours > 99){
+          *hours = 0;
         }
       }
       if(buttonPressed(downButtonPin, &downButtonState)){
-        if(i == 3 && tensPlace - 1 >= 0){
-          tensPlace--;
-          lcd.write('0' + tensPlace);
-        }
-        if(i == 4 && onesPlace - 1 >= 0){
-          onesPlace--;
-          lcd.write('0' + onesPlace);
+        (*hours)--;
+        if(*hours < 0){
+          *hours = 99;
         }
       }
     }
+    snprintf(buffer, 50, format, *hours, *minutes);
+    lcd.print(buffer);
   }
-  *hours = tensPlace * 10 + onesPlace;
+  complete = 0;
+  while (!complete){
+    lcd.setCursor(7, 0);
+    // drop days and hours portion of format for current lcd position
+    char *format = "%02d";
+    if(buttonPressed(enterButtonPin, &enterButtonState)){
+      complete = 1;
+    }
+    else{
+      if(buttonPressed(upButtonPin, &upButtonState)){
+        (*minutes)++;
+        // minutes wrap back around 0 - 59
+        if(*minutes > 59){
+          *minutes = 0;
+        }
+      }
+      if(buttonPressed(downButtonPin, &downButtonState)){
+        (*minutes)--;
+        if(*minutes < 0){
+          *minutes = 59;
+        }
+      }
+    }
+    snprintf(buffer, 50, format, *minutes);
+    lcd.print(buffer);
+  }
 }
 
-void getUVThreshFromLCD(unsigned *uv_thresh){
+/* This function uses an lcd and two buttons as a user interface
+ * to get the uv threshold from the user. This uv sensor threshold
+ * is the point at which the uv lights are activated. If no value
+ * is set then a default threshold is used.
+ */
+void getUVThreshFromLCD(unsigned *uv_thresh, int uv_max){
   if(!uv_thresh){ return; }
   lcd.clear();
-  lcd.print("00");
   lcd.setCursor(0,0);
-  lcd.blink();
-  int value = 0;
-  while(1){
+  lcd.print("00");
+  *uv_thresh = 0;
+  char buffer[50];
+  int complete = 0;
+  // get the uv sensor threshold from user
+  while(!complete){
+    lcd.setCursor(0,0);
+    char *format = "%02d";
     if(buttonPressed(enterButtonPin, &enterButtonState)){
-      break;
+      complete = 1;
     }
     if(buttonPressed(upButtonPin, &upButtonState)){
-      if(value + 1 <= 1023){
-        value++;
+      (*uv_thresh)++;
+      // uv threshold wraps back around 0 - uv max
+      if(*uv_thresh > uv_max){
+        *uv_thresh = 0;
       }
     }
     if(buttonPressed(downButtonPin, &downButtonState)){
-      if(value - 1 >= 0){
-        value--;
-      }
+      (*uv_thresh)--;
+      if(*uv_thresh < 0){
+        *uv_thresh = uv_max;
+      }  
     }
+    snprintf(buffer, 50, format, *uv_thresh);
+    lcd.print(buffer);
   }
-  *uv_thresh = value;
 }
 
 void broadcast(byte *data, unsigned bytes){
@@ -395,20 +437,20 @@ void loop(){;
   }
   else if(programState == SET_UP){
     digitalWrite(greenLedPin, HIGH);
-    char buffer[100];
+    char buffer[50];
     unsigned month, day, year;
-    unsigned hour, minutes, seconds;
+    unsigned hour, minute, second;
     unsigned uv_thresh;
     // get the date and time from the user
     getDateFromLCD(&month, &day, &year);
-    getTimeFromLCD(&hour, &minutes, &seconds);
-    setTime(hour, minutes, seconds, day, month, year);
-    unsigned days, hours;
+    getTimeFromLCD(&hour, &minute, &second);
+    setTime(hour, minute, second, day, month, year);
+    unsigned days, hours, minutes;
     // get the watering schedule (in hours) from the user
-    getScheduleFromLCD(&days, &hours);
-    hours = days * 24 + hours;
+    getScheduleFromLCD(&days, &hours, &minutes);
+    hours = days * 24 + hours + minutes / 60;
     // get the uv threshold from the user
-    getUVThreshFromLCD(&uv_thresh); 
+    getUVThreshFromLCD(&uv_thresh, 30); 
     time_t time = now();
     // broadcast the current time to receivers
     byte *data = (byte*)&time;
